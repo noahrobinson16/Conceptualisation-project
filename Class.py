d3734c0776b984264c8617071b4a993157da5222
noahrobinson16 <2368740@cstjean.qc.ca>
2024-02-15 23:20:30 -0500

    Version 1.1.0

--- Class.py
+++ Class.py
@@ -13,7 +13,6 @@ class Personne:
         birth_month = self.birth_month
         birth_day = self.birth_day
 
-
         def determine_age(birth_year, birth_month, birth_day):
             # Define the birthdate
             birthdate = datetime(birth_year, birth_month, birth_day)
@@ -30,6 +29,8 @@ class Personne:
         days_passed_since_birth = determine_age(birth_year, birth_month, birth_day)
 
         self.age = floor(days_passed_since_birth / 365)
+        self.days_passed_since_birth = days_passed_since_birth
+
 
     def __repr__(self):
         pass
@@ -37,16 +38,67 @@ class Personne:
     def __hash__(self):
         pass
 
+
     def __str__(self):
         return f"-------------------------------" \
                f"\nPrénom: {self.name}" \
                f"\nNom: {self.last_name}" \
                f"\nÂge: {self.age}" \
                f"\nDate de naissance: {self.birth_day}\\{self.birth_month}\\{self.birth_year}" \
+               f"\nNombre de jour passé en vie: {self.days_passed_since_birth}" \
                f"\n-------------------------------"
 
     def __eq__(self, other):
         pass
+
+
+class Job:
+    def __init__(self, prénom:str, nom:str, company: str, heure_ouverture: int, heure_fermeture: int,
+                 temps_plein: bool, jour_embauche: int, mois_embauche: int, année_embauche: int):
+        self.company = company
+        self.heure_ouverture = heure_ouverture
+        self.heure_fermeture = heure_fermeture
+        self.temps_plein = temps_plein
+        self.jour_embauche = jour_embauche
+        self.mois_embauche = mois_embauche
+        self.année_embauche = année_embauche
+        self.prénom = prénom
+        self.nom = nom
+        année_embauche = self.année_embauche
+        mois_embauche = self.mois_embauche
+        jour_embauche = self.jour_embauche
+
+        def determine_day_since_acceptation_to_the_workplace(année_embauche, mois_embauche, jour_embauche):
+            # Define the birthdate
+            date_de_début_au_travail = datetime(année_embauche, mois_embauche, jour_embauche)
+
+            # Get the current date
+            current_date = datetime.now()
+
+            # Calculate the difference in days
+            difference = current_date - date_de_début_au_travail
+            days_passed_since_acceptation_at_the_job = difference.days
+
+            return days_passed_since_acceptation_at_the_job
+
+        days_passed_since_acceptation_at_the_job = determine_day_since_acceptation_to_the_workplace(année_embauche,
+                                                                                                    mois_embauche,
+                                                                                                    jour_embauche)
+        self.jour_depuis_début_emploi = days_passed_since_acceptation_at_the_job
+
+
+
+    def __str__(self):
+        return (f"-------------------------------"
+                f"\nNom complet de l'employé: {self.prénom} {self.nom}"
+                f"\nEmployeur: {self.company}"
+                f"\nHeures d'ouverture: {self.heure_ouverture}:00 à {self.heure_fermeture}:00"
+                f"\nTemps plein: {self.temps_plein}"
+                f"\nNombre de jour à l'emploi: {self.jour_depuis_début_emploi}"
+                f"\n-------------------------------")
+
+
+
 ########################################################################################################################
 
 noah_robinson = Personne("Noah", "Robinson", 2006, 3, 16)
@@ -61,3 +113,5 @@ print(fred_hamel)
 print(sophie_lebel)
 print(emmanuelle_turgeon)
 print(pierre_robinson)
+job_noah_robinson = Job("Noah", "Robinson", "McDonald's", 5, 24, False, 4, 5, 2021)
+print(job_noah_robinson)
