32beb792df9f6b459295decc3fc7391989e7cacc
noahrobinson16 <2368740@cstjean.qc.ca>
2024-02-22 12:29:05 -0500

    Version 4.1.79 math_constante_documentation.py complété

--- mathematical_constant_documentation.py
+++ mathematical_constant_documentation.py
@@ -4,11 +4,162 @@ pi_constant = 3.14159265358979323846
 # Tau
 tau_constant = 6.28318530717958647692
 
-# Pythagoras constant
-# Theodorus constant
-# Phi or Golden ratio
-phi_constant = 1.61803398874989484820
+# Square root of 2, Pythagoras constant
+pythagoras_constant = 1.41421356237309504880
+
+# Square root of 3, Theodorus constant
+theodorus_constant = 1.73205080756887729352
+
+# Square root of 5
+sqrt_5 = 2.23606797749978969640
+
+# Phi, Golden ratio
+golden_ratio = 1.61803398874989484820
+
+# Silver ratio
+silver_ratio = 2.41421356237309504880
+
+# Cube root of 2
+cube_root_2 = 1.25992104989487316476
+
+# Cube root of 3
+cube_root_3 = 1.44224957030740838232
+
+# Twelfth root of 2
+twelfth_root_2 = 1.05946309435929526456
+
+# Super-golden ratio
+supergolden_ratio = 1.46557123187676802665
+
+# Connective constant for the hexagonal lattice
+connective_constant = 1.84775906502257351225
+
+# Kepler-Bouwkamp constant
+kepler_bouwkamp_constant = 0.11494204485329620070
+
+# Wallis's constant
+wallis_constant = 2.09455148154232659148
+
+# Euler's number
+euler_number = 2.71828182845904523536
+
+# Natural logarithm of 2
+ln2 = 0.69314718055994530941
+
+# Lemniscate constant
+lemniscate_constant = 2.62205755429211981046
+
+# Euler's constant
+euler_constant = 057721566490153286060
+
+# Erdos-Borwein constant
+erdos_borwein_constant = 1.60669515241529176378
+
+# Omega constant
+omega_constant = 0.56714329040978387299
+
+# Apéry's constant
+apéry_constant = 1.20205690315959428539
+
+# Laplace limit
+laplace_limit = 0.66274341934918158097
+
+# Ramanujan-Soldner constant
+ramanujan_soldner_constant = 1.45136923488338105028
+
+# Gauss's constant
+gauss_constant = 0.83462684167407318628
+
+# Second Hermite constant
+second_hermite_constant = 1.15470053837925152901
+
+# Liouville's constant
+liouville_constant = 0.110001000000000000000001
+
+# First continued fraction constant
+first_continued_fraction_constant = 0.69777465796400798201
+
+# Ramanujan's constant
+ramanujan_constant = 262537412640768743.999999999999250073
+
+# Glaisher-Kinkelin constant
+glaisher_kinkelin_constant = 1.28242712910062263687
+
+# Catalan's constant
+catalan_constant = 0.91596559417721901505
+
+# Dottie number
+dottie_number = 0.73908513321516064165
+
+# Meissel-Mertens constant
+meissel_mertens_constant = 0.26149721284764278375
+
+# Universal parabolic constant
+universal_parabolic_constant = 2.29558714939263807403
+
+# Cahen's constant
+cahen_constant = 0.64341054628833802618
+
+# Gelfond's constant
+gelfond_constant = 23.1406926327792690057
+
+# Gelfond-Schneider constant
+gelfond_schneider_constant = 2.66514414269022518865
+
+# Second Favard constant
+second_favard_constant = 1.23370055013616982735
+
+# Golden angle
+golden_angle = 2.39996322972865332223
+
+# Sierpinski's constant
+sierpinski_constant = 2.58498175957925321706
+
+# Landau-Ramanujan constant
+landau_ramanujan_constant = 0.76422365358922066299
+
+# First Nielsen-Ramanujan constant
+first_nielsen_ramanujan_constant = 0.82246703342411321823
+
+# Gieseking constant
+gieseking_constant = 1.01494160640965362502
+
+# Bernstein's constant
+bernstein_constant = 0.28016949902386913303
+
+# Tribonacci constant
+tribonacci_constant = 1.83928675521416113255
+
+# Brun's constant
+brun_constant = 1.902160583104
+
+# Twin primes constant
+twin_primes_constant = 0.66016181584686957392
+
+# Plastic ratio
+plastic_ratio = 1.32471795724474602596
+
+# Bloch's constant
 
 
+# Z score for the 97.5 percentile point
+z_score_97_and_a_half_percentile_point = 1.95996398454005423552
 
+# Landau's constant
+
+
+# Landau's third constant
+
+
+# Prouhet-Thue-Morse constant
+prouhet_thue_morse_constant = 0.41245403364010759778
+
+# Golomb-Dickman constant
+golomb_dickman_constant = 0.62432998854355087099
+
+# Constant related to the asymptotic behavior of Lebesgue constants
+constant_related_asymptotic_behavior_lebesgue_constant = 0.98943127383114695174
+
+# Feller-Tornier constant
+feller_tornier_constant = 0.66131704946962233528
 
