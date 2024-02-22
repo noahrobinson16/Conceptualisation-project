653c6f1d99eb0e2c58fc0e2c727843cad2952796
noahrobinson16 <2368740@cstjean.qc.ca>
2024-02-22 10:06:22 -0500

    Version 4.1.2 math_constante_documentation.py complété

---
+++ mathematical_constant_documentation.py
@@ -0,0 +1,14 @@
+# Pi
+pi_constant = 3.14159265358979323846
+
+# Tau
+tau_constant = 6.28318530717958647692
+
+# Pythagoras constant
+# Theodorus constant
+# Phi or Golden ratio
+phi_constant = 1.61803398874989484820
+
+
+
+
